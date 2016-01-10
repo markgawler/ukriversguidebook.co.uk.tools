c2fde0e19721a11a53872f00574ac3ca531167eb
Mark Gawler <markg@wler.co.uk>
2016-01-10 12:49:04 +0000

    update RDS migration script

--- misc-scripts/migration/rds_migration.sh
+++ misc-scripts/migration/rds_migration.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 #The root of the Backup location
-
 RDS_HOST="area51-db.cyhjcpqokgzi.eu-west-1.rds.amazonaws.com"
 RDS_USER="area51"
-RDS_PWD="Magimix1"
+
+# Read Password
+echo -n Database Password: 
+read -s RDS_PWD
+echo
+
 
 SITES_LOCATION="/var/www/ukrgb/"
 FORUM_LOCATION="${SITES_LOCATION}/phpbb"
@@ -43,16 +47,20 @@ echo "CMS user:   ${CMS_DB_USER}"
 
 echo "Creating databases and users"
 (
-mysql -h ${RDS_HOST} -P 3306 -u ${RDS_user} -p${RDS_PWD} <<EOF
+mysql -h ${RDS_HOST} -P 3306 -u ${RDS_USER} -p${RDS_PWD} <<EOF
 DROP DATABASE IF EXISTS ${CMS_DB_NAME};
 DROP DATABASE IF EXISTS ${FORUM_DB_NAME};
+
+DROP USER '${CMS_DB_USER}'@'%';
+DROP USER '${FORUM_DB_USER}'@'%';
+
 
 CREATE DATABASE ${CMS_DB_NAME};
 CREATE DATABASE ${FORUM_DB_NAME};
 
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES, LOCK TABLES ON  ${CMS_DB_NAME}.* TO '${CMS_DB_USER}'@'%' IDENTIFIED BY ${CMS_DB_PWD};
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES, LOCK TABLES ON  ${CMS_DB_NAME}.* TO '${CMS_DB_USER}'@'%' IDENTIFIED BY '${CMS_DB_PWD}';
 
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES, LOCK TABLES ON  ${FORUM_DB_NAME}.* TO '${FORUM_DB_USER}'@'%' IDENTIFIED BY ${CMS_DB_PWD};
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES, LOCK TABLES ON  ${FORUM_DB_NAME}.* TO '${FORUM_DB_USER}'@'%' IDENTIFIED BY '${FORUM_DB_PWD}';
 
 EOF
 )
@@ -62,7 +70,7 @@ EOF
 
 echo "Restore Database"
 echo "Joomla"
-mysql -u ${CMS_DB_NAME} -p${CMS_DB_PWD} ${CMS_DB_NAME} < ~/backups/ukrgb_joomla.sql 
+mysql -h ${RDS_HOST} -P 3306 -u ${CMS_DB_NAME} -p${CMS_DB_PWD} ${CMS_DB_NAME} < ~/backups/ukrgb_joomla.sql 
 
 echo "phpBB"
-mysql -u ${FORUM_DB_NAME} -p${FORUM_DB_PWD} ${FORUM_DB_NAME} < ~/backups/ukrgb_phpBB3.sql
+mysql -h ${RDS_HOST} -P 3306 -u ${FORUM_DB_NAME} -p${FORUM_DB_PWD} ${FORUM_DB_NAME} < ~/backups/ukrgb_phpBB3.sql
