b1caaf1487485d77982ad448b4710f6268ab1433
Mark Gawler <markg@wler.co.uk>
2016-08-05 20:00:14 +0000

    Upgrade Tapatalk version

--- misc-scripts/migration/upgrade_tapatalk.sh
+++ misc-scripts/migration/upgrade_tapatalk.sh
@@ -3,7 +3,7 @@
 # get the location of this script 
 #SRC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-tapatalk_update="tapatalk_phpBB-3.1_v1.3.5.zip"
+tapatalk_update="tapatalk_phpBB-3.1_v1.3.8.zip"
 
 cd $HOME
 wget https://tapatalk.com/files/plugin/${tapatalk_update}
