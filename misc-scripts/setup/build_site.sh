afc0bfcc5a5329898869d9fcbc81b9fb7e93c78f
Mark <markg@wler.co.uk>
2018-10-20 20:58:11 +0100

    site build script development

--- misc-scripts/setup/build_site.sh
+++ misc-scripts/setup/build_site.sh
@@ -3,3 +3,20 @@ cd  ~/ukriversguidebook.co.uk.tools/misc-scripts/backup
 ./site_restore.sh
 ./create_db_users.sh 
 ./site_restore.sh
+
+sudo aws s3 sync s3://backup.ukriversguidebook.co.uk/server-config/apache2/sites-available/ukrgb*.conf \
+	/etc/apache2/sites-available/ --profile backupUser --exclude "*" --include "ukrgb*.conf"
+
+sudo a2ensite ukrgb*
+sudo a2enmod remoteip rewrite expires ssl
+
+
+cd ~/ukriversguidebook.co.uk.tools/CloudFrontIPs
+./get_ips.sh
+sudo cp ip-ranges.lis /var/www/ukrgb/
+
+cd ~/ukriversguidebook.co.uk.tools/misc-scripts/rewritemap
+./buildmap.sh
+
+sudo systemctl restart apache2
+
--- misc-scripts/setup/install_package.sh
+++ misc-scripts/setup/install_package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo add-apt-repository universe
+#sudo add-apt-repository universe
 
 sudo apt-get update
 sudo apt-get upgrade
@@ -8,6 +8,4 @@ sudo apt-get install lamp-server^
 sudo apt-get install php-curl
 sudo apt-get install awscli
 
-sudo a2enmod remoteip rewrite expires
-sudo systemctl restart apache2
 
