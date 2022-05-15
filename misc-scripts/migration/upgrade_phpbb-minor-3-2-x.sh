3f6c11b3dea8f3b3740dc6a91a104a2f79c4ff54
Mark G <markg@wler.co.uk>
2022-05-15 14:43:38 +0000

    Update update scripts

--- misc-scripts/migration/upgrade_phpbb-minor-3-2-x.sh
+++ misc-scripts/migration/upgrade_phpbb-minor-3-2-x.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-new_phpbb_version="3.2.9"
+new_phpbb_version="3.2.11"
 # get the location of this script
 SRC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
--- misc-scripts/migration/upgrade_tapatalk.sh
+++ misc-scripts/migration/upgrade_tapatalk.sh
@@ -3,21 +3,24 @@
 # get the location of this script
 #SRC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-tapatalk_update="tapatalk_phpBB-3.1-3.2_v2.0.5.zip"
+#tapatalk_update="tapatalk_phpBB-3.1-3.2_v2.0.5.zip"
+tapatalk_update="tapatalk_phpBB-3.1-3.2_v2.2.2.zip"
 
-cd $HOME
+cd "$HOME" || exit
 wget https://tapatalk.com/files/plugin/${tapatalk_update}
 
 mkdir tmp
-cd tmp
+cd tmp || exit
 unzip ../${tapatalk_update}
 
 sudo rm -rf /var/www/ukrgb/phpbb/mobiquo
 sudo mv mobiquo /var/www/ukrgb/phpbb/
-cd ext
-sudo rm -rf /var/www/ukrgb/phpbb/ext/tapatalk
-sudo mv tapatalk /var/www/ukrgb/phpbb/ext/
-cd ..
+(
+    cd ext || exit
+    sudo rm -rf /var/www/ukrgb/phpbb/ext/tapatalk
+    sudo mv tapatalk /var/www/ukrgb/phpbb/ext/
+)
+#cd ..
 rmdir ext
 sudo rm -rf /var/www/ukrgb/phpbb.old
 sudo chown -R www-data:www-data /var/www/ukrgb/phpbb/
