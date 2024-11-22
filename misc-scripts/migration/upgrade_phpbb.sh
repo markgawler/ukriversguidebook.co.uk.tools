b46f8c603c45b4766118c78c2d918683e9587f16
Mark Gawler <markg@wler.co.uk>
2024-11-22 07:39:33 +0000

    Fix issue with default version being ignored

--- misc-scripts/migration/upgrade_phpbb.sh
+++ misc-scripts/migration/upgrade_phpbb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 test=false
-new_phpbb_version=3.3.13
+new_phpbb_version="3.3.13"
 while [[ $# -gt 0 ]]; do
 	key=$1
 	case $key in
@@ -33,8 +33,8 @@ while [[ $# -gt 0 ]]; do
 	esac
 done
 
-new_phpbb_major=${version%'.'*}
-new_phpbb_minor=${version##*'.'}
+new_phpbb_major=${new_phpbb_version%'.'*}
+new_phpbb_minor=${new_phpbb_version##*'.'}
 
 # phpBB full version and archive name
 new_phpbb_version="$new_phpbb_major.$new_phpbb_minor"
@@ -43,17 +43,18 @@ phpbb_archive="phpBB-$new_phpbb_version.tar.bz2"
 echo "New phpBB version: $new_phpbb_version"
 #echo "New archive: $new_phpbb_major/$new_phpbb_version/$phpbb_archive"
 
-cd "$HOME" || exit
+#cd "$HOME" || exit
 
 cd /var/www/ukrgb/ || { echo "UKRGB site not found"; exit 1; }
 
 if [ "$test" ]; then
     echo "Starting Upgrade to $new_phpbb_version"
 
-    curl https://download.phpbb.com/pub/release/$new_phpbb_major/$new_phpbb_version/$phpbb_archive -o $phpbb_archive
+    curl https://download.phpbb.com/pub/release/$new_phpbb_major/$new_phpbb_version/$phpbb_archive -o "$HOME/$phpbb_archive"
 
     sudo mv phpbb phpbb.old
-    sudo tar -xjf ~/phpBB-$new_phpbb_version.tar.bz2
+    sudo tar -xjf "$HOME/$phpbb_archive"
+    
     sudo mv phpBB3/ phpbb
     sudo rm phpbb/config.php 
     
@@ -77,6 +78,6 @@ if [ "$test" ]; then
     sudo rm -rf /var/www/ukrgb/phpbb.old
     sudo chown -R www-data:www-data /var/www/ukrgb/phpbb/
 
-    rm "${HOME}"/phpBB-${new_phpbb_version}.tar.bz2
+    rm "$HOME/$phpbb_archive"
 fi
 
